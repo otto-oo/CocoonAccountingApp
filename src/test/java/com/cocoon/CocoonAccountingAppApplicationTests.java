25e8a30ddf2aed3f680ffcbefe54a3c1fd19b800
ececi <huseyinkiceci@gmail.com>
2022-02-04 23:54:54 +0300

    test module included

---
+++ src/test/java/com/cocoon/CocoonAccountingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocoonAccountingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
