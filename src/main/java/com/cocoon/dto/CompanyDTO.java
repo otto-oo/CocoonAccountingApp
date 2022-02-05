aa2db408f0c82568f4087e72eb9ade6703038986
ececi <huseyinkiceci@gmail.com>
2022-02-05 23:52:25 +0300

    CompanyDTO created

---
+++ src/main/java/com/cocoon/dto/CompanyDTO.java
@@ -0,0 +1,17 @@
+package com.cocoon.dto;
+
+
+public class CompanyDTO {
+
+    private Long id;
+    private String title;
+    private String address1;
+    private String address2;
+    private String state;
+    private String zip;
+    private String representative;
+    private String email;
+    private String establishmentDate;
+    private boolean enabled;
+
+}
