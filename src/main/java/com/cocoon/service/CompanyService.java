c33f556c33d232b12178457567a300431cf19cbc
ececi <huseyinkiceci@gmail.com>
2022-02-06 00:11:28 +0300

    CompanyService created

---
+++ src/main/java/com/cocoon/service/CompanyService.java
@@ -0,0 +1,11 @@
+package com.cocoon.service;
+
+import com.cocoon.dto.CompanyDTO;
+
+import java.util.List;
+
+public interface CompanyService {
+
+    public List<CompanyDTO> getAllCompanies();
+
+}
