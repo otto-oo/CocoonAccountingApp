ede0a508a7094b565c068fbf85905f7044b897f5
Sumeyse <sumeyse@gmail.com>
2022-02-08 14:02:05 +0300

    user entity

---
+++ src/main/java/com/cocoon/entity/User.java
@@ -0,0 +1,34 @@
+package com.cocoon.entity;
+
+import lombok.*;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "user")
+@Where(clause = "is_deleted=false")
+public class User extends BaseEntity {
+
+
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String password;
+
+ //   @Enumerated(EnumType.STRING)
+//    private Status status;
+
+    private String phone;
+
+ //   @ManyToOne(fetch = FetchType.LAZY)
+ //   @JoinColumn(name = "role_id")
+ //   private Role role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
+
+}
