5420b96a03e5d0393a9c0141799bc79bc97414ef
Sumeyse <sumeyse@gmail.com>
2022-02-08 18:57:05 +0300

    annotations

--- src/main/java/com/cocoon/repository/UserRepo.java
+++ src/main/java/com/cocoon/repository/UserRepo.java
@@ -2,9 +2,11 @@ package com.cocoon.repository;
 
 import com.cocoon.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
