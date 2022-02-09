65c1fca399f7b18a150cc79614acec13cec0a444
AdamB25 <91336941+AdamB25@users.noreply.github.com>
2022-02-09 20:50:12 +0300

    ACD-25-category-create (#13)
    * Category Entity and CategoryDTO is created
    
    * CategoryController is created
    
    * CategoryService is created
    
    * CategoryRepo is created
    
    * CategoryServiceImpl is created
    
    * Category Entity is corrected
    
    * Category Entity is corrected
    
    * Category DTO is rearranged
    
    * Category Controller create end point is created
    
    * Category Repo is arranged
    
    * CategoryServiceImpl is arranged
    
    * AllArgsCons is gone
    
    * CategoryCotroller create end point done
    
    * CategoryRepo is arranged
    
    * CategoryService is arranged
    
    * CategoryRepo is corrected
    
    * Category create and list functions are done
    
    Co-authored-by: Hüseyin <90786707+khicchi@users.noreply.github.com>

--- src/main/java/com/cocoon/controller/CategoryCotroller.java
+++ src/main/java/com/cocoon/controller/CategoryCotroller.java
@@ -3,6 +3,8 @@ package com.cocoon.controller;
 import com.cocoon.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.stereotype.Controller;
 
@@ -20,7 +22,7 @@ public class CategoryCotroller {
 
     @PostMapping("/create")
     public String createCategory(){
-     return "category/category-add";
+     return "/category/category-add";
     }
 
 
