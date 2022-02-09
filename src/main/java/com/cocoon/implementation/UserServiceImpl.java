fe56625a5cffcd5fb1fa282392c2905b6556a523
Smsay <80817179+smsay@users.noreply.github.com>
2022-02-09 12:30:22 +0300

    method implementations (#19)
    * user entity
    
    * UserDTO
    
    * UserService,Repo,impl
    
    * annotations
    
    * userdto
    
    * method implementations
    
    * properties

--- src/main/java/com/cocoon/implementation/UserServiceImpl.java
+++ src/main/java/com/cocoon/implementation/UserServiceImpl.java
@@ -1,19 +1,33 @@
 package com.cocoon.implementation;
 
 import com.cocoon.dto.UserDTO;
+import com.cocoon.entity.User;
 import com.cocoon.exception.CocoonException;
+import com.cocoon.repository.UserRepo;
 import com.cocoon.service.UserService;
+import com.cocoon.util.MapperUtil;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private UserRepo userRepo;
+    private MapperUtil mapperUtil;
+
+    public UserServiceImpl(UserRepo userRepo, MapperUtil mapperUtil) {
+        this.userRepo = userRepo;
+        this.mapperUtil = mapperUtil;
+    }
+
 
     @Override
     public List<UserDTO> findAllUsers() {
-        return null;
+        List<User> allUsers = userRepo.findAll();
+        return allUsers.stream().map(obj -> mapperUtil.convert(obj, new UserDTO()))
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -23,12 +37,18 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDTO findById(Long id) throws CocoonException {
-        return null;
+        User user = userRepo.findById(id).orElseThrow();
+        if (user == null) {
+            throw new CocoonException("User with " + id + " not exist");
+        }
+        return mapperUtil.convert(user, new UserDTO());
     }
 
     @Override
     public List<UserDTO> listAllUsersByCompanyId(Long id) {
-        return null;
+        List<User> allUsers = userRepo.findAllByCompanyId(id);
+        return allUsers.stream().map(obj -> mapperUtil.convert(obj, new UserDTO()))
+                .collect(Collectors.toList());
     }
 
     @Override
