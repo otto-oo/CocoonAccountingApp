3b0817bc0980d5c3f6c31d61f0c93ed5a599885f
AdamB25 <91336941+AdamB25@users.noreply.github.com>
2022-02-11 18:26:11 +0300

    ACD-27-edit (#36)
    * Category Entity and CategoryDTO is created
    
    * CategoryController is created
    
    * CategoryService is created
    
    * CategoryRepo is created
    
    * CategoryServiceImpl is created
    
    * Category Entity is corrected
    
    * Category Entity is corrected
    
    * Category DTO is rearranged
    
    * Category Controller create end point is created
    
    * Category Repo is arranged
    
    * CategoryServiceImpl is arranged
    
    * AllArgsCons is gone
    
    * CategoryCotroller create end point done
    
    * CategoryRepo is arranged
    
    * CategoryService is arranged
    
    * CategoryRepo is corrected
    
    * Category create and list functions are done
    
    * Category controller is fixed and after master update pushed again
    
    * category add html is fixed
    
    * category list html is fixed
    
    * category edit end point is added
    
    * category edit is done
    
    Co-authored-by: Hüseyin <90786707+khicchi@users.noreply.github.com>

--- src/main/java/com/cocoon/service/CategoryService.java
+++ src/main/java/com/cocoon/service/CategoryService.java
@@ -8,4 +8,7 @@ import java.util.List;
 public interface CategoryService {
     List<CategoryDTO> getAllCategories();
     void save(CategoryDTO categoryDTO) throws CocoonException;
+    CategoryDTO getCategoryByDescription(String description) throws CocoonException;
+    void update(CategoryDTO categoryDTO) throws CocoonException;
+    CategoryDTO getById(Long id);
 }
