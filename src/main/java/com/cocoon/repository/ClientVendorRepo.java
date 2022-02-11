1b61bcb2fc32f00fee6b66010fd9110ce81e7abd
Sumeyse <sumeyse@gmail.com>
2022-02-12 00:08:21 +0300

    ACD-29/30

--- src/main/java/com/cocoon/repository/ClientVendorRepo.java
+++ src/main/java/com/cocoon/repository/ClientVendorRepo.java
@@ -4,7 +4,11 @@ import com.cocoon.entity.ClientVendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClientVendorRepo extends JpaRepository<ClientVendor, Long> {
+
+    ClientVendor findByEmail(String email);
 
 }
