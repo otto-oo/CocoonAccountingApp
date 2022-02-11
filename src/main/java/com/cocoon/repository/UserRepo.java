7c87f6f96d42dcc55bba8b27ac90963fa3f4195c
Sumeyse <sumeyse@gmail.com>
2022-02-11 11:55:26 +0300

    changes

--- src/main/java/com/cocoon/repository/UserRepo.java
+++ src/main/java/com/cocoon/repository/UserRepo.java
@@ -2,12 +2,17 @@ package com.cocoon.repository;
 
 import com.cocoon.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
+
+    @Query(value = "select * from company c\n" +
+            "join users u on c.id = u.company_id\n" +
+            "where c.title = ?1 and u.role_id = 1", nativeQuery = true)
 
     User findByEmail(String email);
 
