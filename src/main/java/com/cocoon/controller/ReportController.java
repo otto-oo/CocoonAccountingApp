d8695bc274ec1f66634238a55063be0c42292d18
mmsezgin <80786426+mmsezgin@users.noreply.github.com>
2022-03-01 21:26:56 +0200

    Stock report display completed (#118)
    * deleteById() created.
    
    * getAllInvoicesSorted() created.
    
    * Dashboard controlller created.
    
    * deleteById is checking product-invoice relation
    
    * Dashboard controller updated
    
    * getStock()
    
    * kicchi commit
    
    * product report binded to ui
    
    Co-authored-by: ececi <huseyinkiceci@gmail.com>

--- src/main/java/com/cocoon/controller/ReportController.java
+++ src/main/java/com/cocoon/controller/ReportController.java
@@ -1,6 +1,11 @@
 package com.cocoon.controller;
 
+import com.cocoon.dto.InvoiceDTO;
 import com.cocoon.dto.InvoiceProductDTO;
+import com.cocoon.entity.InvoiceProduct;
+import com.cocoon.repository.InvoiceProductRepo;
+import com.cocoon.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +17,28 @@ import java.util.ArrayList;
 @RequestMapping("/report")
 public class ReportController {
 
+
+    private ProductService productService;
+    private InvoiceService invoiceService;
+    private InvoiceProductService invoiceProductService;
+    private InvoiceProductRepo invoiceProductRepo;
+
+    public ReportController(InvoiceService invoiceService, ProductService productService,
+                            InvoiceProductService invoiceProductService, InvoiceProductRepo invoiceProductRepo) {
+        this.invoiceService = invoiceService;
+        this.productService = productService;
+        this.invoiceProductService = invoiceProductService;
+        this.invoiceProductRepo = invoiceProductRepo;
+    }
+
     @GetMapping("/stock")
     public String getStock(Model model){
-        model.addAttribute("stock", new ArrayList<InvoiceProductDTO>());
+
+        ArrayList<InvoiceProduct> stock = (ArrayList<InvoiceProduct>) invoiceProductRepo.getStockReportListProducts();
+//        ArrayList<InvoiceProductDTO> stock = invoiceProductService.getStockReportList();
+
+        model.addAttribute("stock", stock);
+
         return "report/stock-report.html";
     }
 }
