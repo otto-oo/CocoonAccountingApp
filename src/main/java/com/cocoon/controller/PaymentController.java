dd068b517cdc7c8d6df7553c00e96d8c3bc9954c
otto <otto.o@cybertekschool.com>
2022-03-01 14:41:09 +0300

    some changes for payment

--- src/main/java/com/cocoon/controller/PaymentController.java
+++ src/main/java/com/cocoon/controller/PaymentController.java
@@ -1,43 +1,42 @@
 package com.cocoon.controller;
 
-import com.cocoon.entity.payment.AuthorizationResponse;
 import com.cocoon.entity.payment.Payment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 
 @RestController
 public class PaymentController {
 
     private WebClient webClient = WebClient.builder().baseUrl("https://api.yapily.com").build();
-    //private WebClient webClient = WebClient.builder().baseUrl("http://localhost:8080").build();
 
-    @GetMapping("/payments")
-    public Mono<AuthorizationResponse> createWebClient() {
+    @PostMapping("/payments")
+    public Mono<Object> createWebClient() {
         Payment payment1 = new Payment();
         System.out.println("payment1.toString() = " + payment1.toString());
         return webClient
                 .post()
-                .uri("https://api.yapily.com/payment-auth-requests")
+                .uri("/payment-auth-requests")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .header("Authorization", "Basic ODZlM2ZmZWEtNjFkOC00MTQ5LTk2NmMtM2YzMjFiZWJhYTEyOmZkYTdkZGFlLTE5OWEtNDM3ZS1iMTRkLTI2ZmRjNGI2MmU4Nw==")
                 .body(Mono.just(payment1), Payment.class)
                 .retrieve()
-                .bodyToMono(AuthorizationResponse.class);
+                .bodyToMono(Object.class);
     }
 
     @GetMapping("/payments/{id}/details")
-    public Mono<Payment> readMonoWithWebClient(@PathVariable("id") String id){
+    public Mono<Object> readMonoWithWebClient(@PathVariable("id") String id){
         return webClient
                 .get()
                 .uri("/payments/{id}/details", id)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header("Authorization", "Basic ODZlM2ZmZWEtNjFkOC00MTQ5LTk2NmMtM2YzMjFiZWJhYTEyOmZkYTdkZGFlLTE5OWEtNDM3ZS1iMTRkLTI2ZmRjNGI2MmU4Nw==")
+                .header("Consent", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJJTlNUSVRVVElPTiI6Im1vZGVsby1zYW5kYm94IiwiQ09OU0VOVCI6ImE4ZTRmZTAxLTQ2ZmMtNDAwYy05OTdjLWNjMzMyNzEzOGY2ZiIsIkFQUExJQ0FUSU9OX1VTRVJfSUQiOiJzaW5nbGUtcGF5bWVudC10dXRvcmlhbCIsIlVTRVIiOiIzZWM1YTYxNy0zNDdhLTQ5ZTctOTIxYS1mZGY5MjlmNjY4ZWEifQ.v2BI0ry1ogn2qnvIChaHx3Y3MEjZ-E3FCdnmwHVp2MlRfoC-9ZD9ZHxeuSF02T_DgC1E-EuR82243U9PcnceVw")
                 .retrieve()
-                .bodyToMono(Payment.class);
+                .bodyToMono(Object.class);
 
     }
 
