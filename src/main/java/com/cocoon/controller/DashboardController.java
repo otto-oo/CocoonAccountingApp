8ee2255662c8f379f96575be2003cefc1401f4f9
Smsay <80817179+smsay@users.noreply.github.com>
2022-03-02 19:14:38 +0300

    more changes (#121)
    * changes in html
    
    * ACD-29/30
    
    * ACD-29/30
    
    * ACD-29/30 html
    
    * minor change
    
    * minor change
    
    * minor changes
    
    * minor changes
    
    * minor changes
    
    * edit pattern
    
    * edit pattern
    
    * zipcpde pattern
    
    * ACD-10 Login
    
    * Enter Key
    
    * Enter Key no need
    
    * authorizations
    
    * some changes
    
    * more changes
    
    Co-authored-by: owermaiden <93406612+owermaiden@users.noreply.github.com>

--- src/main/java/com/cocoon/controller/DashboardController.java
+++ src/main/java/com/cocoon/controller/DashboardController.java
@@ -1,27 +1,25 @@
 package com.cocoon.controller;
 
 import com.cocoon.dto.InvoiceDTO;
-import com.cocoon.dto.ProductDTO;
-import com.cocoon.service.ClientVendorService;
+import com.cocoon.service.CompanyService;
 import com.cocoon.service.InvoiceService;
-import com.cocoon.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
 
-
+     private  final    CompanyService companyService;
     private final InvoiceService invoiceService;
 
-    public DashboardController(InvoiceService invoiceService) {
+    public DashboardController(CompanyService companyService, InvoiceService invoiceService) {
+        this.companyService = companyService;
         this.invoiceService = invoiceService;
     }
 
@@ -33,6 +31,7 @@ public class DashboardController {
         List<InvoiceDTO> updatedInvoices = invoices.stream().map(invoiceService::calculateInvoiceCost).collect(Collectors.toList());
         model.addAttribute("invoices", updatedInvoices);
         model.addAttribute("result", invoiceService.calculateTotalProfitLoss());
+        model.addAttribute("companyName",companyService.getCompanyByLoggedInUser());
 
         return "dashboard";
     }
