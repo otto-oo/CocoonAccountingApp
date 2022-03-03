8092f7d6d86ac63a5255127722405f646de31baa
ececi <huseyinkiceci@gmail.com>
2022-03-03 20:23:53 +0300

    currency rates are fetched and viewed

---
+++ src/main/java/com/cocoon/dto/currency/Rate.java
@@ -0,0 +1,10 @@
+package com.cocoon.dto.currency;
+
+import java.io.Serializable;
+
+public class Rate implements Serializable {
+    public double USD;
+    public double GBP;
+    public double RUB;
+    public double JPY;
+}
