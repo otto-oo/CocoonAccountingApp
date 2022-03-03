1310d7d5720789ebb6d685084e2e27ce5edd3df6
otto <otto.o@cybertekschool.com>
2022-03-03 23:08:41 +0300

    payment service, impl and repo created..

---
+++ src/main/java/com/cocoon/implementation/PaymentServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cocoon.implementation;
+
+import com.cocoon.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+
+}
