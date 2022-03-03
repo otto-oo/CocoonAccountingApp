6eafbdcaa69d06d0d2c05516459ae1b466d36078
owermaiden <93406612+owermaiden@users.noreply.github.com>
2022-03-03 15:58:09 +0300

    otto (#128)
    * invoice service
    invoice repo
    mapperUtil
    invoice entity
    invoice Dto
    invoice controller
    is connected to each other...
    
    * invoice service
    invoice repo
    mapperUtil
    invoice entity
    invoice Dto
    invoice controller
    is connected to each other...
    
    * invoice - entity
            - DTO
            - repository
            - service
            - implementation
            - enums
            - controller added...
    
    * invoice-product manytomany
    
    * invoice-product manytomany
    
    * some changes
    
    * some changes
    
    * update html added
    
    * update html added
    
    * update html added
    
    * update funtionalty
    
    * update funtionalty
    
    * smart crud
    
    * smart crud
    
    * smart crud
    
    * Delete application.properties
    
    * invoice_product entity added
    
    * invoice_product entity added
    
    * invoice_product entity added
    
    * invoice_product entity works fine...
    
    * invoice_product entity works fine...
    
    * invoice_product entity works fine...
    
    * delete functionaltiy added
    
    * delete functionaltiy updated
    
    * ower
    
    * delete Invoiceproduct added
    
    * CRUD finished
    
    * CRUD finished
    
    * minor bugs
    
    * minor bugs
    
    * approve functionality added
    
    * minor changes
    
    * to invoice started
    
    * some bugs
    
    * some bugs
    
    * toInvoice updated
    
    * some performance update
    
    * some performance update
    
    * product quantity update added
    
    * basic security added
    
    * basic security added
    
    * basic security added
    
    * basic security added
    
    * toinvoice ok
    
    * product count automated...
    
    * super changes
    
    * profit loss calculation added
    
    * profit loss calculation added
    
    * PDF invoice added
    
    * PDF invoice added
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    * some changes
    
    Co-authored-by: Adam GIT <92720727+CrazyDevo@users.noreply.github.com>
    Co-authored-by: Hüseyin <90786707+khicchi@users.noreply.github.com>
    Co-authored-by: otto <85919400+otto-oo@users.noreply.github.com>
    Co-authored-by: Baha <59250233+bahatas@users.noreply.github.com>
    Co-authored-by: cy-ali <92939861+cy-ali@users.noreply.github.com>

--- src/main/java/com/cocoon/controller/PaymentController.java
+++ src/main/java/com/cocoon/controller/PaymentController.java
@@ -1,21 +1,33 @@
 package com.cocoon.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/payment")
 public class PaymentController {
 
-    /*@GetMapping("/list")
+    List<String> payments = new ArrayList<String>(Arrays.asList("hi","my"));
 
-    @GetMapping("/newpayment/{id}")
+    @GetMapping("/list")
+    public String createPayment(Model model) {
 
-    @PostMapping("/newpayment/{consentId}")
+        model.addAttribute("payments",payments);
+        return "payment/payment-list";
+    }
 
-    @GetMapping("/newpayment/complete")*/
+//    @GetMapping("/newpayment/{id}")
+//
+//    @PostMapping("/newpayment/{consentId}")
+//
+//    @GetMapping("/newpayment/complete")*/
 
 
 
