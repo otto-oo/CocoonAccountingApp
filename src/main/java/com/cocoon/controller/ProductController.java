68ca53fcd751d7405ced42281ba3c08fbb0dc887
otto <otto.o@cybertekschool.com>
2022-03-03 22:19:40 +0300

    changes for product add

--- src/main/java/com/cocoon/controller/ProductController.java
+++ src/main/java/com/cocoon/controller/ProductController.java
@@ -37,8 +37,6 @@ public class ProductController {
         model.addAttribute("productStatus", ProductStatus.values());
         model.addAttribute("unit", Unit.values());
         model.addAttribute("categories", categoryService.getAllCategories());
-        // TODO @otto There should be an attribute to call user's company, this has to be done after security portion
-        // TODO @otto Just manager can add product according to picture in SRS.
         return "product/product-add";
     }
 
