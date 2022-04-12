32a0a27f66bbbbc8154ec7342bc95259837adbfa
owermaiden <ower.maiden@cybertekschool.com>
2022-04-12 18:37:31 +0300

    Stripe done

--- src/main/java/com/cocoon/controller/PaymentController.java
+++ src/main/java/com/cocoon/controller/PaymentController.java
@@ -1,11 +1,13 @@
 package com.cocoon.controller;
 
 import com.cocoon.dto.PaymentDTO;
-import com.cocoon.entity.Payment;
 import com.cocoon.exception.CocoonException;
 import com.cocoon.entity.common.ChargeRequest;
+import com.cocoon.implementation.StripeServiceImpl;
 import com.cocoon.service.CompanyService;
 import com.cocoon.service.PaymentService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,10 +21,12 @@ public class PaymentController {
     @Value("${STRIPE_PUBLIC_KEY}")
     private String stripePublicKey;
 
+    private final StripeServiceImpl stripeServiceImpl;
     private final PaymentService paymentService;
     private final CompanyService companyService;
 
-    public PaymentController(PaymentService paymentService, CompanyService companyService) {
+    public PaymentController(StripeServiceImpl stripeServiceImpl, PaymentService paymentService, CompanyService companyService) {
+        this.stripeServiceImpl = stripeServiceImpl;
         this.paymentService = paymentService;
         this.companyService = companyService;
     }
@@ -59,6 +63,30 @@ public class PaymentController {
         model.addAttribute("payment", paymentService.getPaymentById(id));
         model.addAttribute("company", companyService.getCompanyByLoggedInUser());
 
+        return "payment/payment-success";
+    }
+
+    // charge controller
+
+    @PostMapping("/charge/{id}")
+    public String charge(ChargeRequest chargeRequest, @PathVariable("id") Long id, Model model)
+            throws StripeException {
+        chargeRequest.setDescription("Example charge");
+        chargeRequest.setCurrency(ChargeRequest.Currency.EUR);
+        Charge charge = stripeServiceImpl.charge(chargeRequest);
+        PaymentDTO dto = paymentService.updatePayment(id);
+        model.addAttribute("id", charge.getId());
+        model.addAttribute("status", charge.getStatus());
+        model.addAttribute("chargeId", charge.getId());
+        model.addAttribute("balance_transaction", charge.getBalanceTransaction());
+        model.addAttribute("company", companyService.getCompanyByLoggedInUser());
+        model.addAttribute("payment", dto);
+        return "payment/payment-success";
+    }
+
+    @ExceptionHandler(StripeException.class)
+    public String handleError(Model model, StripeException ex) {
+        model.addAttribute("error", ex.getMessage());
         return "payment/payment-success";
     }
 
